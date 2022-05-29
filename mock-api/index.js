5e4b6d8c9d0296fcbfce0fe2fc89aa641391be81
BaoLopTruong <ngocbaole1999@gmail.com>
2022-05-29 13:10:08 +0700

    update port 3

--- mock-api/index.js
+++ mock-api/index.js
@@ -3,10 +3,13 @@ const apiMocker = require("connect-api-mocker");
 const cors = require("cors");
 
 const app = express();
-const port = "https://baoloptruong.github.io/mock-api";
+const port=3001;
+const hostname= "https://baoloptruong.github.io/mock-api";
 
 app.use(cors({ origin: true, credentials: true }));
 
 app.use("/api", apiMocker("mock-api"));
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+// app.listen(port, () => console.log(`Server listening on port ${port}!`));
+
+app.listen(hostname, () => console.log(`Server listening on port ${port}!`));
